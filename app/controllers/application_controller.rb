83cf9136343de17f961d37bcdc2cefb3a5754348
Charles R. Portwood II <charlesportwoodii@erianna.com>
2017-02-09 15:53:19 -0600

    Storing changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::API
     require 'hmac_signature_auth'
     protected
         def auth
-            if HMACSignatureAuth.new.authenticate(request) == false
+            if HMACSignatureAuth.authenticate(request) == false
                 return render :status => :unauthorized
             end
 
--- app/filters/auth/hmac_signature_auth.rb
+++ app/filters/auth/hmac_signature_auth.rb
@@ -1,6 +1,7 @@
 class HMACSignatureAuth
     require 'securerandom'
-    
+    require "base64"
+
     DATE_HEADER = 'X-DATE'
     AUTHORIZATION_HEADER = 'Authorization'
     DRIFT_TIME_ALLOWANCE = 90
@@ -8,6 +9,18 @@ class HMACSignatureAuth
     AUTH_INFO = 'HMAC|AuthenticationKey'
 
     def self.authenticate(request)
+
+        date = request.headers[self::DATE_HEADER]
+        authorization = request.headers[self::AUTHORIZATION_HEADER].sub('HMAC ', '').split(',')
+
+        if (authorization.length != 3) 
+            return false
+        end
+
+        access_token = authorization[0]
+        hmac = Base64.decode64(authorization[1])
+        salt = Base64.decode64(authorization[2])
+
         return true
     end
 end
